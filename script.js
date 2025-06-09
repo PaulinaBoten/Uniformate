ec9f831c1992bf56967af1a315eb3834680304e3
oohproyecto <you@example.comecho>
2025-06-09 14:52:25 -0500

    Actualizacion carpetas

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//var (global)nombre de la variable;//
+//let (no global) nombre variable solo en uno en que este definido//
+//const (no se puede modifiar) nombre de la variable//
+
 console.log("Hola");
 const boton = document.getElementById("boton");
 boton.addEventListener("click", function(){
