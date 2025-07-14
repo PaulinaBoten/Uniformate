9c3454841f8d94a2e705ff9479b265e036050f6d
frank <frankruiz_90@hotmail.com>
2025-07-14 14:37:47 -0500

    bloqueo

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ console.log("Hola");
 const boton = document.getElementById("boton");
 boton.addEventListener("click", function(){
     console.log("Estoy dentro del boton");
-});
+})
+
+localStorage
