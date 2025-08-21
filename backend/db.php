0a0e4bc32b9753409f756e864022d52dc6189956
oohproyecto <you@example.comecho>
2025-08-21 17:40:03 -0500

    actual bd

--- backend/db.php
+++ backend/db.php
@@ -6,6 +6,6 @@ $dbname = "chochete";
 
 $conn = new mysqli($host, $user, $pass, $dbname, 3306);
 if ($conn->connect_error) {
-    die ("Error de conexion;" . $conn->connect_error);
+    die ("Error de conexion:" . $conn->connect_error);
 }
 ?>
